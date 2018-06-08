1eb5646551fe1da91f68c7d2b54d7c9464a4412a
sanjaysir <sanjay.sirohee@gmail.com>
2018-06-08 14:40:48 +0530

    some Chnages done

--- application/modules/plant_master/views/add_member_right_tpl.php
+++ application/modules/plant_master/views/add_member_right_tpl.php
@@ -18,11 +18,11 @@
       </div>
 		<?php
 		// MySQL connect info
-			//mysql_connect("localhost", "tpdbuser", "india@123");
-			//mysql_select_db("trackingprortaldb21");
+			mysql_connect("localhost", "tpdbuser", "india@123");
+			mysql_select_db("trackingprortaldb");
 			
 			//mysql_connect("localhost", "root", "");
-			//mysql_select_db("trackingprortaldb20");
+			//mysql_select_db("trackingportaldb");
 			
 			
 			//$query = mysql_query("SELECT MAX( plant_id ) FROM plant_master;");
